69f961513b3f4170ff3735c623d5210a97e93d6e
VictorMuramoto <victor.muramoto@gmail.com>
2014-12-02 23:54:58 -0500

    Reader data.txt - Adding data to Counter and Dict

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 # -*- coding: utf-8 -*-
-import json
-from pprint import pprint
-json_data = open('AllSets.json')
-data = json.load(json_data)
-pprint(data)
-json_data.close     
+#import json
+#from pprint import pprint
+#json_data = open('AllSets.json')
+#data = json.load(json_data)
+#pprint(data)
+#json_data.close         
+
+from collections import Counter
+
+f = open('data.txt')
+count = Counter()
+i = 0
+transactions = dict()
+for line in f:
+    line = line.strip()    
+    if i == 0:
+        url = line
+    if i == 31:
+        print 'this is a count'
+        transactions[url] = count        
+        print transactions[url]
+        count.clear()
+        i= 0
+    count[line] += 1
+    i = i +1
+    #for x in range(1,32):
+     #   print line
+    
+
+    
