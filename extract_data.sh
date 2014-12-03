45fe0e4d194010f322cb8768888d153d0db8f5d4
Hugo Cabral Tannús <hugoct.ufg@gmail.com>
2014-12-03 00:48:23 -0600

    Input data reformatted.

--- extract_data.sh
+++ extract_data.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
-aux=$(date -v-30d "+%Y %m %d")
+out='data.txt'
+aux=$(date -v-3d "+%Y %m %d")
 _yyyy=$(echo $aux | cut -d' ' -f1)
 _mm=$(echo $aux | cut -d' ' -f2)
 _dd=$(echo $aux | cut -d' ' -f3)
 
-home_path="http://hearthstats.net/decks/public?utf8=%E2%9C%93&q[klass_id_eq]=1&q[unique_deck_created_at_gteq]=$_yyyy-$_mm-$_dd+00%3A00%3A00+UTC&q[unique_deck_num_matches_gteq]=35&q[name_cont]=&items=100&sort=winrate&order=desc"
-echo $home_path
+rm $out
 
-url_list=$(wget -q $home_path -O - | grep '<td class="name">' | cut -d\" -f4)
+echo Any Druid Hunter Mage Paladin Priest Rogue Shaman Warlock Warrior >> $out
 
-for url in $url_list;
+for i in {1..9};
 do
-	echo url: $url
-	url_path="http://hearthstats.net$url"
-	echo $url_path >> data.txt
-	deck_name=$(wget -q $url_path -O - | grep 'col-md-12' | cut -d\> -f5 | cut -d\< -f1) >> data.txt
-	echo $deck_name
-	#for line in $deck_name;
-	#do
-	#	echo $line
-	#done
-	wget -q $url_path -O - | grep 'blizz_id' | cut -d\" -f6 >> data.txt
+	home_path="http://hearthstats.net/decks/public?utf8=%E2%9C%93&q[klass_id_eq]=$i&q[unique_deck_created_at_gteq]=$_yyyy-$_mm-$_dd+00%3A00%3A00+UTC&q[unique_deck_num_matches_gteq]=35&q[name_cont]=&items=100&sort=winrate&order=desc"
+	
+	count=$(wget -q $home_path -O - | grep '<td class="name">' | wc -l)
+	echo $count
+	echo $count >> $out
+	
+	url_list=$(wget -q $home_path -O - | grep '<td class="name">' | cut -d\" -f4)
+
+	for url in $url_list;	
+	do
+		url_path="http://hearthstats.net$url"
+		deck_name=$(wget -q $url_path -O - | grep 'col-md-12' | cut -d\> -f5 | cut -d\< -f1)
+		#deck_cost=$(wget -q $url_path -O - | grep 'class="deck-details row"' | cut -d\> -f36 | cut -d\< -f1)
+		echo $deck_name
+		echo $deck_name >> $out
+		deck_cards=$(wget -q $url_path -O - | grep 'blizz_id' | cut -d\" -f6) 
+		echo $deck_cards >> $out
+	done
 done
