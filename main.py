ee6c4ef30db5ff4ddfa264e8e63dd4c63830f2b3
Hugo Cabral Tannús <hugoct.ufg@gmail.com>
2014-12-03 20:32:38 -0600

    Data set containing winrate

--- main.py
+++ main.py
@@ -10,25 +10,32 @@ import sys
 import apriorialg as ap
 from collections import Counter
 
+
+winrate = list()
+winrate.append([])
+
+arg = sys.argv[1:]
+
 def load_dataset():
 	f = open('data.txt')
 	heroes = f.readline().strip().split()
 	dataset = list()
 	for k in range(1,10):
 		N = int(f.readline().strip())
+		rate = f.readline().strip().split()
+		winrate.append(rate)
 		for i in range(N):
 			name = f.readline().strip()
-			line = f.readline().strip().split()
-			c = Counter(line)
-			transaction = c.items()
-			transaction.insert(0,heroes[k]);
+			cards = f.readline().strip().split()
+			
+			counter = Counter(cards)
+			transaction = counter.items()
+			transaction.append(heroes[k]);
 			dataset.append(transaction)
-	
+		
 	return dataset
 
-arg = sys.argv[1:]
-
-print arg
+img_domain = 'https://s3-us-west-2.amazonaws.com/hearthstats/cards/'
 
 minsupport = float(arg[0])
 min_confidence = float(arg[1])
@@ -40,10 +47,17 @@ rules = ap.generateRules(L, support_data, min_confidence)
 
 print '\nL:'
 for l in L:
-	print " * ", l, "\n"
+	print " * ", l
+
 #print '\nSupport data:\n', support_data
 print '\nRules:'
 for rule in rules:
-	print " - ", rule, "\n"
+	print " - ", rule
+
 		
-    
+# argumentos para a DP:
+# - eliminar todos os itens de L que contenham alguma carta que não pertença ao deck
+# - peso da mochila é igual à quantidade de cartas inseridas
+# - V será medido pela radiciação de grau igual a quantidade de itemsets sobre o valor da 
+# multiplicação do support de cada itemset envolvido.
+# - o resultado da D.P. dará o valor da sinergia / coerência do Deck. 
